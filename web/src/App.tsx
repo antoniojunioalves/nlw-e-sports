5111942364f0298ba81921d1c133f9f7eca20d1a
antoniojunioalves <antoniojunioalves@gmail.com>
2022-09-13 21:11:30 -0300

    feat: aula 2 - layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,82 @@
-interface ButtonProps {
-  title: string;
-}
+import "./styles/main.css";
+import LogoImg from './assets/logo-nlw-esports.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
-function Button(props: ButtonProps) {
+function App() { 
   return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={LogoImg} />
 
-function App() {
-  return (
-    <div>
-      <Button title= "Button1" />
-      <Button title= "Button2" />
-      <Button title= "Button3" />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png"/>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Legue of Legends</strong>
+            <span className="text-zinc-300 text-sm block" >4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center ">
+          <div>
+            <strong className="text-2xl text-white front-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
